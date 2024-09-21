3e41ba2b0f0a71a34d90c7426437760cf3b3a98e
difadev <difadev@gmail.com>
2024-09-21 11:54:18 +0700

    feat: [TSR-000] Add gitignore env

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,14 @@
 export const login = async (req, res) => {
     try {
       const {fullName, username, password, confirmPassword, gender} = req.body;
+      if(password !== confirmPassword) {
+        return res.status(400).json({error: "Password don;t match"})
+      }
+      const user = await User.findOne({username});
+      if(user) {
+        return res.status(400).json({error: "Username already exists"})
+      }
+      // Hash Password
     } catch(error) {
       
     }
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,11 @@ import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
 const app =express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World !!")
-})
 
+dotenv.config();
+
+app.use(expres.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
